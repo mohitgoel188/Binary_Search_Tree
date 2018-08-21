68e767061e6339f982ed74d0e2bb06e1f4f656a9
mohitgoel188 <35655238+mohitgoel188@users.noreply.github.com>
2018-08-22 00:45:18 +0530

    Added new traversal option
    Spiral traversal Now available.
    Depth and traverse have thier own option now.

--- BST.py
+++ BST.py
@@ -1,19 +1,19 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self,datum):
         self.left=self.right=None
-        self.data=data
+        self.datum=datum
 
 class BST:
 
-    def insert(self,data,root):
+    def insert(self,datum,root):
         if root==None:
-            return Node(data)
+            return Node(datum)
         else:
-            if data<=root.data:
-                new=self.insert(data,root.left)
+            if datum<=root.datum:
+                new=self.insert(datum,root.left)
                 root.left=new
-            if data>root.data:
-                new=self.insert(data,root.right)
+            if datum>root.datum:
+                new=self.insert(datum,root.right)
                 root.right=new
         return root
 
@@ -28,23 +28,71 @@ class BST:
                 r=1+self.depth(root.right)
             return max(l,r)
 
-    def bfs(self,root):             #Breath First Search
+    def bfs(self,root,mode='parse',searchitem=None):             #Breath First Search
         queue=[root]
         while len(queue)!=0:
             r=queue.pop(0)
-            print(r.data,end=' ')
+            if mode=='search':
+                if r.datum==searchitem:
+                    return r
+            else:
+                print(r.datum,end=' ')
             if r.left!=None:
                 queue.append(r.left)
             if r.right!=None:
                 queue.append(r.right)
 
     def dfs(self,root):             #Depth First Search
-        print(root.data,end=' ')
+        print(root.datum,end=' ')
         if root.left!=None:
             self.dfs(root.left)
         if root.right!=None:
             self.dfs(root.right)
 
+    def sts(self,left_helix=True,root=None,mode='parse',searchItem=None):   #Spiral Traversal/Search
+        if left_helix:
+            if root!=None:
+                self.lh=[root]
+                self.rh=[]
+            while len(self.lh)>0:
+                exNode=self.lh.pop()
+                if mode=='parse':
+                    print(exNode.datum,end=' ')
+                else:
+                    if exNode.datum==searchItem:
+                        return exNode
+                if exNode.right!=None:
+                    self.rh.append(exNode.right)
+                if exNode.left!=None:
+                    self.rh.append(exNode.left)
+            if len(self.rh)>0:
+                self.sts(False)
+        else:
+            if root!=None:
+                self.lh=[]
+                self.rh=[root]
+            while len(self.rh)>0:
+                exNode=self.rh.pop()
+                if mode=='parse':
+                    print(exNode.datum,end=' ')
+                else:
+                    if exNode.datum==searchItem:
+                        return exNode
+                if exNode.left!=None:
+                    self.lh.append(exNode.left)
+                if exNode.right!=None:
+                    self.lh.append(exNode.right)
+            if len(self.lh)>0:
+                self.sts(True)
+
+    def delete(self,root,item):
+        node=self.bfs(root,mode='search',searchitem=item)
+        if node.left==None:
+            node.datum=0
+        while node.left.left != None:
+            node.datum=node.left.datum
+        node.datum=node.left.datum
+        node.left=None
 
 def main():
     tree=BST()
@@ -53,10 +101,33 @@ def main():
     print('Enter values of Node:')
     root=None
     for i in range(L):
-        data=int(input())
-        root=tree.insert(data,root)
-    print(f'\nDepth Of Tree {tree.depth(root)}\nBFS: ',end='')
-    tree.dfs(root)
+        datum=float(input())
+        root=tree.insert(datum,root)
+    while True:
+        choice=int(input('\n\n\tBST Operations Menu:-\n1. Insert\n2. Delete\n3. Traverse\n4. Depth\n\nEnter your choice: '))
+        if choice==1:
+            datum=input('Enter Value: ')
+            root=tree.insert(float(datum),root)
+        elif choice==2:
+            datum=input('Enter value of node to be deleted: ')
+        elif choice==3:
+            way=int(input('\nHow does the parsing happen:\n1. Breadth First\n2. Depth First\n3. Spiral\n\nEnter your choice: '))
+            if way==1:
+                print('BFS: ',end='')
+                tree.bfs(root)
+            elif way==2:
+                print('DFS: ',end='')
+                tree.dfs(root)
+            elif way==3:
+                helical=input('Helix-> Left first(l)\\Right first(r): ')
+                if helical.lower()=='r':
+                    left_helix=False
+                else:
+                    left_helix=True
+                print('\nSpiral: ',end='')
+                tree.sts(left_helix,root)
+        elif choice==4:
+            print(f'\nDepth Of Tree {tree.depth(root)}')
 
 if __name__ == '__main__':
     main()
